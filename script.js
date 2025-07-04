829fb237593187a17bc152e825160ffaf0e5e862
Yanis Blanchard <yanis.blanchard@gmail.com>
2025-07-04 12:30:39 +0200

    étape 4 - c'est le drame....

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let Formulaire = document.querySelector('form')
+Formulaire.addEventListener('click', Formulaire) 
+    const text = document.getElementsByClassName("quote").value;
+    const author = document.getElementsByClassName("name").value;
+
+console.log(text, author)
+
